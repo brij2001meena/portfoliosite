ab62472814abdf1f8c79d0e4ca6cc7a92ee5c102
brij2001meena <73888737+brij2001meena@users.noreply.github.com>
2021-07-05 23:07:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots', 
+    });
+    let hamerger =document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobileNav = document.querySelector('.mobile-nav');
+    hamerger.addEventListener('click',function(){
+        mobileNav.classList.add('open');
+
+    });
+    times.addEventListener('click',function(){
+        mobileNav.classList.remove('open');
+    });
+
+});
